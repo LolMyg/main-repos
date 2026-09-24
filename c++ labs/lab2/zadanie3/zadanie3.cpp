8c8f57837ab4941ec7af0acbda482d88fe013d1b
LolMyg <ispirianov@mail.ru>
2026-09-24 19:15:24 +0300

    задание 3 лабораторная 2

--- c++ labs/lab2/zadanie3/zadanie3.cpp
+++ c++ labs/lab2/zadanie3/zadanie3.cpp
@@ -1,1 +1,13 @@
-﻿
+﻿#include <iostream>
+using namespace std;
+int main() {
+    setlocale(LC_ALL, "ru");
+    double a, b, c, d, e, x;
+    cout << "Введите коэффициенты a, b, c, d, e: ";
+    cin >> a >> b >> c >> d >> e;
+    cout << "Введите значение x: ";
+    cin >> x;
+    // Схема Горнера:
+    double y = ((((a * x + b) * x + c) * x + d) * x + e);
+    cout << y;
+}
