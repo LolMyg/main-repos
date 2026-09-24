f7d4e110bc53848a9b05f792302d572d62db72e7
LolMyg <ispirianov@mail.ru>
2026-09-24 18:23:00 +0300

    сделал задание 3 лабораторная 3

--- c++ labs/lab3/zadanie3/zadanie3.cpp
+++ c++ labs/lab3/zadanie3/zadanie3.cpp
@@ -1,1 +1,24 @@
-﻿
+﻿#include <iostream>
+#include <windows.h> // Добавил для работы с кодировкой Windows
+using namespace std;
+int main() {
+	// Сменил кодировку
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	setlocale(LC_ALL, "ru");
+	string color;
+	cout << "Какой горит цвет? ";
+	cin >> color;
+	if (color == "green" || color == "зеленый") {
+		cout << "идите";
+	}
+	else if (color == "yellow" || color == "желтый") {
+		cout << "ждите";
+	}
+	else if (color == "red" || color == "красный") {
+		cout << "стойте";
+	}
+	else {
+		cout << "некоректный ввод";
+	}
+}
